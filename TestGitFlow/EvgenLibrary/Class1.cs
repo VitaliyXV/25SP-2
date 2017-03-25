952c524ca5984a27fed699a2159035bcafa2863d
Evgeniy <einstain77@gmail.com>
2017-03-25 17:46:25 +0200

    Do method witch return random string

---
+++ TestGitFlow/EvgenLibrary/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvgenLibrary
+{
+    public class StringMaker
+    {
+        string str1 = "Hello!";
+        string str2 = "How are you?";
+        string str3 = "Goodluck!";
+
+        public string GetString()
+        {
+            Random r = new Random(2);
+            int x = r.Next();
+            if (x == 0)
+            {
+                return str1;
+            }
+            else if (x == 1)
+            {
+                return str2;
+            }
+            else
+            {
+                return str3;
+            }
+
+
+        }
+    }
+}
--- TestGitFlow/TestGitFlow/Program.cs
+++ TestGitFlow/TestGitFlow/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EvgenLibrary;
+using System;
 
 namespace TestGitFlow
 {
@@ -11,6 +8,8 @@ namespace TestGitFlow
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            StringMaker sm = new StringMaker();
+            Console.WriteLine(sm.GetString());
         }
     }
 }
