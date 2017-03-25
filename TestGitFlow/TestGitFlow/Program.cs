7e07abf43ea608549ee3f1fccd6cd816e3e1b356
Иван Жигалкин <ivan.zhigalkin@gmail.com>
2017-03-25 17:38:08 +0200

    Adding Ivan lib

--- TestGitFlow/TestGitFlow/Program.cs
+++ TestGitFlow/TestGitFlow/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IvanLib;
 
 namespace TestGitFlow
 {
@@ -10,7 +11,9 @@ namespace TestGitFlow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IvanLib.IvanLib ivan = new IvanLib.IvanLib();
+
+            Console.WriteLine("Hello World! " + ivan.GetData());
         }
     }
 }
