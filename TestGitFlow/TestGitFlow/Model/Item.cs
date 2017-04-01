e6bc996a4e0de8b6c5102d7ce1d77974c6d500e6
Иван Жигалкин <ivan.zhigalkin@gmail.com>
2017-04-01 17:59:01 +0300

    Added Item, IItemClonable, Category

---
+++ TestGitFlow/TestGitFlow/Model/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitFlow.Model
+{
+    abstract class Item : IItemClonable
+    {
+        public string Name { get; protected set; }
+        public float Price { get; protected set; }
+        public CategoryType Category { get; protected set; }
+    }
+}
