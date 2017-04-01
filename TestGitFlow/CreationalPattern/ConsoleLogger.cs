ab6c3cde150dfbd7e1b6fca40e2e3578cdcc8768
Короляков Евгений Олегович <25127@step.edu>
2017-04-01 16:39:39 +0300

    Added color messages into console logger

--- TestGitFlow/CreationalPattern/ConsoleLogger.cs
+++ TestGitFlow/CreationalPattern/ConsoleLogger.cs
@@ -7,23 +7,33 @@ namespace CreationalPattern
     {
         public void Info(string message)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(DateTime.Now + "; " + "Info: " + message);
+            Console.ForegroundColor = ConsoleColor.White;
         }
         public void Debug(string message)
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(DateTime.Now + "; " + "Debug: " + message);
+            Console.ForegroundColor = ConsoleColor.White;
         }
         public void Warning(string message)
         {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
             Console.WriteLine(DateTime.Now + "; " + "Warning: " + message);
+            Console.ForegroundColor = ConsoleColor.White;
         }
         public void Error(string message)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(DateTime.Now + "; " + "Error: " + message);
+            Console.ForegroundColor = ConsoleColor.White;
         }
         public void Fail(string message)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(DateTime.Now + "; " + "Fail: " + message);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
