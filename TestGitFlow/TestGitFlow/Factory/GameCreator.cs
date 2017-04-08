7f941bb93cf9d08e744a4403fb2470df3f5969cd
SergB <21178@step.edu>
2017-04-08 14:38:41 +0300

    + Game Creator

---
+++ TestGitFlow/TestGitFlow/Factory/GameCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestGitFlow.Model;
+
+namespace TestGitFlow.Factory
+{
+    class GameCreator:IItemCreator    
+    {
+        private Game newgame;
+
+        public void GameCreator()
+        {
+            newgame = new Game();
+        }
+
+        public Item CreateItem()
+        {            
+            return newgame.Clone();
+        }
+    }
+}
