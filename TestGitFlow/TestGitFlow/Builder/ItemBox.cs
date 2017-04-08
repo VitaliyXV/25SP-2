67af765ad3c2c038156d071cb550008d841c7556
Короляков Евгений Олегович <25127@step.edu>
2017-04-08 16:26:36 +0300

    Added IBuider, ItemBox

---
+++ TestGitFlow/TestGitFlow/Builder/ItemBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestGitFlow.Model;
+
+namespace TestGitFlow.Builder
+{
+    public class ItemBox
+    {
+        public List<Item> Items { get; set; }
+
+        public ItemBox()
+        {
+            Items = new List<Item>();
+        }
+
+    }
+}
