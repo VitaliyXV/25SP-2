a1a3043c96a1907daf3d71d3c6d963abca084538
Короляков Евгений Олегович <25127@step.edu>
2017-04-08 17:22:31 +0300

    Added IItemComponent, remake Item

---
+++ TestGitFlow/TestGitFlow/Compozit/IItemComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitFlow.Compozit
+{
+    public interface IItemComponent
+    {
+        void AddChild(IItemComponent itemComponent);
+        void RemoveChild(IItemComponent itemComponent);
+        IItemComponent GetChildList();
+    }
+}
