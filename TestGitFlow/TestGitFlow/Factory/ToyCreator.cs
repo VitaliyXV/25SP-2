748ef528d5603a6f47f0208f88e36e320a08605d
Иван Жигалкин <ivan.zhigalkin@gmail.com>
2017-04-08 14:34:39 +0300

    Toy creator realization

--- TestGitFlow/TestGitFlow/Factory/ToyCreator.cs
+++ TestGitFlow/TestGitFlow/Factory/ToyCreator.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestGitFlow.Model;
 
 namespace TestGitFlow.Factory
 {
-    class ToyCreator
+    class ToyCreator: IItemCreator
     {
+        public Item CreateItem()
+        {
+           return new Toy();
+        }
     }
 }
