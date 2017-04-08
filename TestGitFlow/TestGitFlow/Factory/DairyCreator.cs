255020ccf800ecb7a618499548b3d511a8237f90
Миняйло Александр Николаевич <25129@step.edu>
2017-04-08 15:37:15 +0300

    Alex DairyCreator

---
+++ TestGitFlow/TestGitFlow/Factory/DairyCreator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestGitFlow.Model;
+
+namespace TestGitFlow.Factory
+{
+    class DairyCreator: IItemCreator
+    {
+        private Dairy _baseDairy;
+
+        public DairyCreator(Dairy newDairy)
+        {
+            _baseDairy = newDairy;
+        }
+
+        public Item CreateItem()
+        {
+            return _baseDairy.Clone();
+        }
+
+        public Type GetItemType()
+        {
+            return _baseDairy.GetType();
+        }
+    }
+}
