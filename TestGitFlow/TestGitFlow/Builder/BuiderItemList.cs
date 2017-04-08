861ec79bde40808e3fde8b5dd9eeb7ba5b2ee63a
Короляков Евгений Олегович <25127@step.edu>
2017-04-08 16:49:26 +0300

    Added BuilderItemList

---
+++ TestGitFlow/TestGitFlow/Builder/BuiderItemList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestGitFlow.Model;
+
+namespace TestGitFlow.Builder
+{
+    class BuiderItemList : IBuilder
+    {
+        private ItemBoxList itemList;
+
+        public void AddItem(Item item)
+        {
+            if (itemList == null) itemList = new ItemBoxList();
+            itemList.Items.Add(item);
+            
+        }
+
+        public IItemBox GetItemBox()
+        {
+            return itemList;
+        }
+    }
+}
