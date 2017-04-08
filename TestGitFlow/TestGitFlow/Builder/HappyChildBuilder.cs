203778d7902cd1db265de6ad77590d8a0901a296
Иван Жигалкин <ivan.zhigalkin@gmail.com>
2017-04-08 16:40:04 +0300

    Happy child builder

---
+++ TestGitFlow/TestGitFlow/Builder/HappyChildBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestGitFlow.Model;
+
+namespace TestGitFlow.Builder
+{
+    public class HappyChildBuilder: IBuilder
+    {
+        private ItemBox _box;
+        public HappyChildBuilder()
+        {
+            _box = new ItemBox();
+        }
+
+        public void AddItem(Item item)
+        {
+            _box.Items.Add(item);
+        }
+
+        public ItemBox GetItemBox()
+        {
+            return _box;
+        }
+    }
+}
