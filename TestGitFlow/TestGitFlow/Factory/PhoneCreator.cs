b2a2a9d46c6aecb7e4d161570a59132a6a3d6744
Короляков Евгений Олегович <25127@step.edu>
2017-04-08 14:36:22 +0300

    Add PhoneCreator, remake class Phone(added defoult constructor)

---
+++ TestGitFlow/TestGitFlow/Factory/PhoneCreator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestGitFlow.Model;
+
+namespace TestGitFlow.Factory
+{
+    class PhoneCreator : IItemCreator
+    {
+        private Phone phone;
+
+        public PhoneCreator()
+        {
+            this.phone = new Phone();
+        }
+        public PhoneCreator(Phone phone)
+        {
+            this.phone = phone;
+        }
+
+        public Item CreateItem()
+        {
+            return phone.Clone();
+        }
+    }
+}
