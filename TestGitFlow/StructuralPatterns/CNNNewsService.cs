fb90282fccc8757f51be9895bb31e6fa7f784d9a
SergB <21178@step.edu>
2017-04-15 14:56:42 +0300

    CNNNewsService

---
+++ TestGitFlow/StructuralPatterns/CNNNewsService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralPatterns
+{
+    class CNNNewsService
+    {
+        List<CNNNews> allnews;
+
+        public List<CNNNews> GetNews()
+        {
+            return allnews;
+        }
+
+        public void AddNews(CNNNews newone)
+        {
+            allnews.Add(newone);
+        }
+
+        public string GetNewsText()
+        {
+            string stemp = "";
+            foreach(CNNNews news in allnews)
+            {
+                stemp += news.ToString();
+            }
+            return stemp;
+        }
+
+    }
+}
